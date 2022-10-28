d5dc2d83ee9f3c92a5d7b08f4b199f56ee9ebc30
dawn <tang.d@hotmail.com>
2022-10-28 16:58:20 +0800

    Use find_namespace_packages in spiir setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="spiir",
     version="0.0.2",
-    packages=find_packages(where="src"),
+    packages=find_namespace_packages(where="src", include=["spiir.*"]),
     package_dir={"": "src"},
     python_requires=">=3.8",
     install_requires=[
-        "spiir.data",
-        "spiir.io",
-        "spiir.search",
-        "spiir.workflow",
+        "wheel",
+        "setuptools",
+        "lalsuite",
+        "astropy",
+        "python-ligo-lw>=1.8.1",
+        "ligo.skymap",
+        "igwn-alert",
+        "ligo-gracedb",
+        "toml",
+        "numpy",
+        "scipy",
+        "pandas",
+        "matplotlib",
+        "click",
     ],
+    extras_require={
+        "pycbc": ["pycbc"],
+    },
     description="A Python library for the SPIIR gravitational wave search pipeline.",
     author="Daniel Tang",
     author_email="daniel.tang@uwa.edu.au",
