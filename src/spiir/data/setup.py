40314f26e9134b0d1aabddc1e6b1a931d9e41ec9
dawn <tang.d@hotmail.com>
2022-10-28 16:58:07 +0800

    edit subpackage setup.py to find subsubpackages

--- src/spiir/data/setup.py
+++ src/spiir/data/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="spiir.data",
     version="0.0.1",
-    # package_dir={"": "src"},
+    packages=find_packages(),
     python_requires=">=3.8",
     install_requires=[
         "wheel",
--- src/spiir/search/setup.py
+++ src/spiir/search/setup.py
@@ -3,8 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="spiir.search",
     version="0.0.1",
-    # packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
     python_requires=">=3.8",
     install_requires=[
         "wheel",
