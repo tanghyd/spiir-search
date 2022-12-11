9a19cb24f83856026888c2316a8fa7f8e0b9b7a0
tanghyd <tang.d@hotmail.com>
2022-12-11 23:41:43 +1300

    Add prelim bayes factor code from Victor Oloworaran

--- src/spiir/search/p_astro/__init__.py
+++ src/spiir/search/p_astro/__init__.py
@@ -1,3 +1,3 @@
-from . import mchirp_area, models
+from . import bayes_factor, mchirp_area, models
 from .mchirp_area import ChirpMassAreaModel
 from .models import CompositeModel, TwoComponentModel
